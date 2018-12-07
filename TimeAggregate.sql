47d0638d7ce1d836a0be5da91257baf432a0caeb
diptarupSaha <45678772+diptarupSaha@users.noreply.github.com>
2018-12-07 10:06:06 +0530

    Update TimeAggregate.sql

--- TimeAggregate.sql
+++ TimeAggregate.sql
@@ -1,20 +1,22 @@
 /*
 	Scenario:
 				
+				
 		Sample Table
-		--------------------------------------------------------
-		ID   |       STATE				|     ELECTRICAL_OUTAGE
-		--------------------------------------------------------
-		1    |			Karnataka			|      4316:52:00
-		2    |			Karnataka			|        16:01:00
-		3    |			Karnataka			|       345:23:00
-		4		 |			Rajasthan			|			   56:23:00
-		5		 |			Rajasthan			|				 12:00:00
-		6		 |			Rajasthan			|				 32:00:00
-		--------------------------------------------------------
+		---------------------------------------------------
+		ID   |       STATE	|     ELECTRICAL_OUTAGE
+		---------------------------------------------------
+		1    |	Karnataka	|      4316:52:00
+		2    |	Karnataka	|        16:01:00
+		3    |	Karnataka	|       345:23:00
+		4    |	Rajasthan	|        56:23:00
+		5    |	Rajasthan	|	 12:00:00
+		6    |	Rajasthan	|	 32:00:00
+		---------------------------------------------------
 		
 		
 		Find the Total Hours & Minutes of Electrical Outage for the each state
+		As seconds value as 00 here I have considered only HH:MM
 */
 
 SELECT
