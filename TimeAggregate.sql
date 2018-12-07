76c73be3f9a96b995e0c4b7cb4561c389f61fc05
diptarupSaha <45678772+diptarupSaha@users.noreply.github.com>
2018-12-07 10:11:47 +0530

    Update TimeAggregate.sql

--- TimeAggregate.sql
+++ TimeAggregate.sql
@@ -2,21 +2,21 @@
 	Scenario:
 				
 				
-		Sample Table
-		---------------------------------------------------
-		ID   |       STATE	|     ELECTRICAL_OUTAGE
-		---------------------------------------------------
-		1    |	Karnataka	|      4316:52:00
-		2    |	Karnataka	|        16:01:00
-		3    |	Karnataka	|       345:23:00
-		4    |	Rajasthan	|        56:23:00
-		5    |	Rajasthan	|	 12:00:00
-		6    |	Rajasthan	|	 32:00:00
-		---------------------------------------------------
+		Sample Table: ELECTRICAL_OUTAGE
+		-----------------------------------------------------------
+		ID   |	CITY		|  STATE		|     OUTAGE_DURATION
+		-----------------------------------------------------------
+		1    |	Hubli		|	Karnataka	|      4316:52:00
+		2    |	Belagavi	|	Karnataka	|        16:01:00
+		3    |	Belgaum		|	Karnataka	|       345:23:00
+		4    |	Jaipur		|	Rajasthan	|        56:23:00
+		5    |	Jodhpur		|	Rajasthan	|		 12:00:00
+		6    |	Jaisalmer	|	Rajasthan	|	 	 32:00:00
+		-----------------------------------------------------------
 		
 		
 		Find the Total Hours & Minutes of Electrical Outage for the each state
-		As seconds value as 00 here I have considered only HH:MM
+		As seconds value are 00 here I have considered only HH:MM
 */
 
 SELECT
@@ -25,13 +25,13 @@ SELECT
 
       /* TIME CALCULATION STARTS HERE */
 			
-      (CAST(SUM(CAST(SUBSTR_AFTER(LEFT(LPAD("ELECTRICAL_OUTAGE",10,'0'),7),':') as integer))/60 AS INTEGER) 
+      (CAST(SUM(CAST(SUBSTR_AFTER(LEFT(LPAD("ELECTRICAL_OUTAGE",10,'0'),7),':') AS INTEGER))/60 AS INTEGER) 
       + CAST(SUM(CAST(SUBSTR_BEFORE("ELECTRICAL_OUTAGE",':') AS INTEGER)) AS INTEGER)) AS "OUTAGE_DURATION_HOUR",
 			
-      MOD(SUM(CAST(SUBSTR_AFTER(LEFT(LPAD("ELECTRICAL_OUTAGE",10,'0'),7),':') as integer)),60) as "OUTAGE_DURATION_MIN"
+      MOD(SUM(CAST(SUBSTR_AFTER(LEFT(LPAD("ELECTRICAL_OUTAGE",10,'0'),7),':') AS INTEGER)),60) as "OUTAGE_DURATION_MIN"
 			
       /*TIME CALCULATION ENDS HERE */
 
 FROM 
-      "SCHEMA"."TABLE_NAME"
-group by "STATE";
+      "<SCHEMA>"."ELECTRICAL_OUTAGE"
+GROUP BY "STATE";
